a612e36d6237f29cbc1ee620dfcc228c540c0efd
Amr <amr@hp-pc.Home>
2018-06-25 01:46:03 +0200

    adding the main file which will contain the program

---
+++ DioAdcSerialINTERRUPT/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 25, 2018
+ *      Author: amr
+ */
+
+#include "types.h"
+#include "Ports_definitions.h"
+#include "DIO.h"
+#define F_CPU 8000000UL
+#include <util/delay.h>
+
+
+int main(){
+	while(1){
+
+	}
+}
