ee7b1998199f8b1925d26ffeb7efdb9fc5a98286
Amr <amr@hp-pc.Home>
2018-06-25 15:44:27 +0200

    editing the main with the ADC ISA and initializing the ADC

--- DioAdcSerialINTERRUPT/main.c
+++ DioAdcSerialINTERRUPT/main.c
@@ -8,25 +8,46 @@
 #include "types.h"
 #include "Ports_definitions.h"
 #include "DIO.h"
+#include "ADC.h"
 #include <avr/interrupt.h>
 #define F_CPU 8000000UL
 #include <util/delay.h>
 
 int main(){
-	//initializing DIO
+	//initializing DIO and ADC
 	DIO_Initialize();
+	ADC_Initialize();
+
 	//setting the global interrupt
 	sei();
+
+	//select ADC MUX
+	ADC_MuxSelection(ZERO);
+	//enable ADC interrupt
+	ADC_EnableINT();
+	//8-bit or 10-bit resolution
+	ADC_Adjustment(RIGHT);
+	//adjust pre_scaler value
+	ADC_PescalerValue(128);
+	//turn on the ACD
+	ADC_EnableADC();
+	ADC_StartConversion();
+
 	ExtInt_Enable((u8_t)ZERO);
 	ExtINT_SenseControl((u8_t)TWO,(u8_t)ZERO);//on both edges for Interrupt 0 sense control
 	while(1){
-		/*
-		 * do the program here
-		 */
 	}
 }
 
 ISR(INT0_vect)//external interrupt 1
 {
 	DIO_ToggleBit(PortD, SEVEN);
+	//TODO send the led status to the serial monitor
+}
+
+ISR(ADC_vect, ISR_NOBLOCK)
+{
+	u16_t ADCVal = ADC_getVal();
+	//TODO add the following line in the ISA of the timer
+	//ADC_StartConversion();
 }
